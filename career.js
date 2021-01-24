cfce90952439a4d7f2f20f9ae7e9500e077f8723
Saurabh Patel <64309848+saurabhpatel7@users.noreply.github.com>
2021-01-24 12:07:43 +0530

    Add files via upload

---
+++ career.js
@@ -0,0 +1,5 @@
+var mq = document.getElementsByTagName("marquee");
+var last = document.getElementById("last");
+if(last)
+    // check the postion of last. if its positioning the down border: then direction:down; else direction:up
+    
